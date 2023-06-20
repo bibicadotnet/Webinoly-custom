ee2fc8722e02687fed79d331f3652c8fb5bf507d
bibicadotnet <119997727+bibicadotnet@users.noreply.github.com>
2023-06-20 10:46:50 +0700

    Update webinoly.sh

--- webinoly.sh
+++ webinoly.sh
@@ -7,4 +7,6 @@ wget -qO weby qrok.es/wy && sudo bash weby -clean
 wget https://raw.githubusercontent.com/bibicadotnet/Webinoly-custom/main/webinoly.conf -O /opt/webinoly/webinoly.conf
 # Install Nginx and LEMP
 sudo stack -lemp
+# tat firewall
+sudo apt remove iptables-persistent -y && sudo ufw disable && sudo iptables -F
 echo "<<< ГОТОВО >>>"
