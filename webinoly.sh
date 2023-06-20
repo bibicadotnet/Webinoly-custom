8bd9df321c0adfd4af58c6cdc4993adbd3c99767
bibicadotnet <119997727+bibicadotnet@users.noreply.github.com>
2023-06-20 15:18:43 +0700

    Update webinoly.sh

--- webinoly.sh
+++ webinoly.sh
@@ -6,7 +6,7 @@ sudo apt install screen -y;
 sudo wget -qO weby qrok.es/wy && sudo bash weby -clean;
 sudo wget --no-check-certificate "https://raw.githubusercontent.com/bibicadotnet/Webinoly-custom/main/webinoly.conf" -O /opt/webinoly/webinoly.conf;
 sudo stack -nginx=mainline -build=light;
-sudo stack -mysql-ver=10.11;
+sudo stack -mysql;
 sudo stack -php -build=light;
 sudo apt remove iptables-persistent -y;
 sudo ufw disable && sudo iptables -F;
