ca5148ec7e98652e543b0fc3ec2771d587e83e03
bibicadotnet <119997727+bibicadotnet@users.noreply.github.com>
2023-06-20 13:56:29 +0700

    Update webinoly.sh

--- webinoly.sh
+++ webinoly.sh
@@ -8,11 +8,11 @@ wget https://raw.githubusercontent.com/bibicadotnet/Webinoly-custom/main/webinol
 sudo stack -nginx
 sudo stack -php
 sudo stack -mysql
-sudo stack -letsencrypt -purge
-sudo stack -backups -purge
-sudo stack -postfix -purge
-sudo stack -redis -purge
-sudo stack -memcached -purge
+sudo stack -letsencrypt -purge -y
+sudo stack -backups -purge -y
+sudo stack -postfix -purge -y
+sudo stack -redis -purge -y
+sudo stack -memcached -purge -y
 sudo apt remove iptables-persistent -y
 sudo ufw disable
 sudo iptables -F
