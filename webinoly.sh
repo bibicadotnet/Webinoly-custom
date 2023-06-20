307381f9257d5253e6ed27a6bf175b8b940a1c33
bibicadotnet <119997727+bibicadotnet@users.noreply.github.com>
2023-06-20 14:21:44 +0700

    Update webinoly.sh

--- webinoly.sh
+++ webinoly.sh
@@ -3,10 +3,10 @@ sudo apt install zip -y
 sudo apt install unzip -y
 sudo apt install htop -y
 sudo apt install screen -y
-wget -qO weby qrok.es/wy && sudo bash weby -clean
+wget -qO weby qrok.es/wy && sudo bash weby -clean -build=light
 wget https://raw.githubusercontent.com/bibicadotnet/Webinoly-custom/main/webinoly.conf -O /opt/webinoly/webinoly.conf
-sudo stack -nginx
-sudo stack -php-ver=8.2
+sudo stack -nginx -build=light
+sudo stack -php-ver=8.2 -build=light
 sudo stack -mysql
 sudo stack -letsencrypt -purge -y
 sudo stack -backups -purge -y
