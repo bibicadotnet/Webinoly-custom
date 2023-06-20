278cb878e519138be332bfa37f3ec26213318401
bibicadotnet <119997727+bibicadotnet@users.noreply.github.com>
2023-06-20 19:42:14 +0700

    Update webinoly.sh

--- webinoly.sh
+++ webinoly.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 wget -qO weby qrok.es/wy && sudo bash weby -clean
-curl https://raw.githubusercontent.com/bibicadotnet/Webinoly-custom/main/webinoly.conf -O /opt/webinoly/webinoly.conf
+curl -O https://raw.githubusercontent.com/bibicadotnet/Webinoly-custom/main/webinoly.conf -O /opt/webinoly/webinoly.conf
 sudo stack -lemp -build=light
 sudo stack -mysql
 sudo apt remove iptables-persistent -y
