c4cd6767e5936580af1c3b5a2f751079cdd49560
bibicadotnet <119997727+bibicadotnet@users.noreply.github.com>
2023-06-20 13:04:28 +0700

    Update webinoly.sh

--- webinoly.sh
+++ webinoly.sh
@@ -3,11 +3,11 @@ sudo apt install zip -y
 sudo apt install unzip -y
 sudo apt install htop -y
 sudo apt install screen -y
-timedatectl set-timezone Asia/Ho_Chi_Minh
 wget -qO weby qrok.es/wy && sudo bash weby -clean
+wget https://raw.githubusercontent.com/bibicadotnet/Webinoly-custom/main/webinoly.conf -O /opt/webinoly/webinoly.conf
 sudo stack -nginx=mainline
 sudo stack -php-ver=7.4
-sudo stack -mysql-ver=10.3.39
+sudo stack -mysql
 sudo apt-get remove memcached -y
 sudo apt-get remove redis-server -y
 sudo apt remove iptables-persistent -y
