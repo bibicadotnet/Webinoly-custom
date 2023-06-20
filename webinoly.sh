8a5d19337651dbed54a2fdaee77926e87f6cbb00
bibicadotnet <119997727+bibicadotnet@users.noreply.github.com>
2023-06-20 11:12:32 +0700

    Update webinoly.sh

--- webinoly.sh
+++ webinoly.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sudo wget -qO weby qrok.es/wy && sudo bash weby -clean && wget https://raw.githubusercontent.com/bibicadotnet/Webinoly-custom/main/webinoly.conf -O /opt/webinoly/webinoly.conf && sudo stack -lemp && sudo apt remove iptables-persistent -y && sudo ufw disable && sudo iptables -F
+sudo wget https://raw.githubusercontent.com/bibicadotnet/Webinoly-custom/main/webinoly.conf -O /opt/webinoly/webinoly.conf && wget -qO weby qrok.es/wy && sudo bash weby && sudo apt remove iptables-persistent -y && sudo ufw disable && sudo iptables -F
