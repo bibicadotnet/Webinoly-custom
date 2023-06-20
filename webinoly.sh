ada43b4892707526548d0e910eb53dd4128e9873
bibicadotnet <119997727+bibicadotnet@users.noreply.github.com>
2023-06-20 17:56:35 +0700

    Update webinoly.sh

--- webinoly.sh
+++ webinoly.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 wget -qO weby qrok.es/wy && sudo bash weby -clean
+rm /opt/webinoly/webinoly.conf
 sudo wget --no-check-certificate "https://raw.githubusercontent.com/bibicadotnet/Webinoly-custom/main/webinoly.conf" -O /opt/webinoly/webinoly.conf
 sudo stack -lemp -build=light
 sudo stack -mysql -purge=force
