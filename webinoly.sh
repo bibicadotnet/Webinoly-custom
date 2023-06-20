54cb6b5a8caae63a61a51d131e413f6dfe026cad
bibicadotnet <119997727+bibicadotnet@users.noreply.github.com>
2023-06-20 11:16:09 +0700

    Update webinoly.sh

--- webinoly.sh
+++ webinoly.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
+sudo apt install zip -y
+sudo apt install unzip -y
+sudo apt install htop -y
+sudo apt install screen -y
 sudo wget https://raw.githubusercontent.com/bibicadotnet/Webinoly-custom/main/webinoly.conf -O /opt/webinoly/webinoly.conf && wget -qO weby qrok.es/wy && sudo bash weby && sudo apt remove iptables-persistent -y && sudo ufw disable && sudo iptables -F
