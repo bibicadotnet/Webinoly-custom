d42b9a6a605a1e2d23dc7bee68afba7ae4ac814b
bibicadotnet <119997727+bibicadotnet@users.noreply.github.com>
2023-06-20 19:36:10 +0700

    Update webinoly.sh

--- webinoly.sh
+++ webinoly.sh
@@ -1,24 +1,14 @@
 #!/bin/bash
-# cài đặt webinoly sạch
 wget -qO weby qrok.es/wy && sudo bash weby -clean
-# chép file config
 sudo wget --no-check-certificate "https://raw.githubusercontent.com/bibicadotnet/Webinoly-custom/main/webinoly.conf" -O /opt/webinoly/webinoly.conf
-# cài đặt php, mysql, nginx
 sudo stack -lemp -build=light
-# xóa mysql 10.6 mặc định
-sudo stack -mysql -purge=force
-# cài mysql 10.11
-sudo stack -mysql-ver=10.11
 sudo stack -mysql
-# tắt firewall cho Oracle Ubuntun
 sudo apt remove iptables-persistent -y
 sudo ufw disable
 sudo iptables -F
-# cài đặt zip. unzip, htop, screen
 sudo apt install zip -y
 sudo apt install unzip -y
 sudo apt install htop -y
 sudo apt install screen -y
-# kiểm tra lại thông tin webinoly
 sudo webinoly -verify
 sudo webinoly -info
