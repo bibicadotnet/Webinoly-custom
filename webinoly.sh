982dc76619b38a12d901b46ab76364e5b9a749c8
bibicadotnet <119997727+bibicadotnet@users.noreply.github.com>
2023-06-20 14:47:40 +0700

    Update webinoly.sh

--- webinoly.sh
+++ webinoly.sh
@@ -5,7 +5,7 @@ sudo apt install htop -y
 sudo apt install screen -y
 sudo wget --no-check-certificate -qO weby qrok.es/wy && sudo bash weby -clean
 sudo wget --no-check-certificate "https://raw.githubusercontent.com/bibicadotnet/Webinoly-custom/main/webinoly.conf" -O /opt/webinoly/webinoly.conf
-sudo stack -nginx -build=light
+sudo stack -nginx=mainline -build=light
 sudo stack -mysql-ver=11.0
 sudo stack -php-ver=8.2 -build=light
 sudo apt remove iptables-persistent -y
